e53d0d21fe916e53d8adec87e7a56765518de530
Yohanna Philip  Abana <yohanaabana02@gmail.com>
2022-02-21 13:49:23 +0100

    Setup projects

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <h1 class="display-1">{{num}}</h1>
+  <button class="btn btn-success" @click="count">Count</button>
+</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const num = ref(0);
+
+const count = () => {
+  num.value++;
+}
+</script>
+
+<style>
+
+</style>
